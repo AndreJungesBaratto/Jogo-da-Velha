8401db544c7566d6ade32ef469ca474dba7b0772
AndreJungesBaratto <102363867+AndreJungesBaratto@users.noreply.github.com>
2022-05-16 23:02:13 -0300

    Jogo da velha funcional as imperfeito
    Prós: pelo menos tá "funcionando"; não tiro zero mais
    Contras: desorganizado pra krl; desorganizado esmo; difícil de entender; algumas coisas pra arrumar que só vai da pra arrumar quando eu separar tudo em funções.

--- jogo da velha.c
+++ jogo da velha.c
@@ -1,57 +1,90 @@
 #include <stdio.h>
+//a função teste irá averiguar se houve vencedor
+int teste(int jogo[3][3]){
+    for(int l=0; l<3; l++){
+        if(jogo[l][0]==jogo[l][1]&&jogo[l][1]==jogo[l][2]&&jogo[l][0]!='.'){
+            return 2;
+        }
+        else continue;
+    }
 
-#define LIN 3
-#define COL 3
+    for(int c=0;c<3;c++){
+        if(jogo[0][c]==jogo[1][c]&&jogo[1][c]==jogo[2][c]&&jogo[0][c]!='.'){
+            return 2;
+        }
+        else continue;
+    }
+
+    if(jogo[0][0]==jogo[1][1]&&jogo[1][1]==jogo[2][2]&&jogo[0][0]!='.'){
+        return 2;
+    }
+    if(jogo[0][2]==jogo[1][1]&&jogo[1][1]==jogo[2][0]&&jogo[0][2]!='.'){
+        return 2;
+    }
+
+    for(int l=0; l<3; l++){
+        for(int c=0; c<3; c++){
+            if(jogo[l][c]=='.'){
+                return 1;
+                break;
+            }
+        }
+    }
+}
 
 void main(){
-    int jogo[LIN][COL];
 
-    printf("Bem vindo ao Jogo da Velha! \nPara jogar, digite qual o simbolo que deseja jogar (X ou O) e aperte enter. \n");
-    printf("Em seguida, digite a coordenada em que deseja jogar na forma 'linha, coluna'.\n");
+    int jogo[3][3];
 
-    int velha=0;
-    int L, C;
+    printf("Bem vind@s ao Jogo da Velha! \nPara jogar, digite qual o simbolo que ira comecar o jogo (X ou O) e aperte enter. \n");
+    printf("Em seguida, cada jogador digitara as coordenadas em que deseja jogar na forma 'linha (enter) coluna (enter)'\n");
+
+    int L, C, velha=1;
     char simb;
 
-    for(int l=0; l<LIN; l++){
-        for(int c=0; c<COL; c++){
+
+    for(int l=0; l<3; l++){
+        for(int c=0; c<3; c++){
             jogo[l][c]='.';
         }
     }
 
-    while(velha==0){
-
-        scanf("%s", &simb);
-
-        if(simb!='x'&&simb!='X'&&simb!='o'&&simb!='O'){
+    scanf("%s", &simb);
+        while(simb!='X'&&simb!='x'&&simb!='o'&&simb!='O'){
             printf("Digite um simbolo valido!\n");
-            continue;
+            scanf("%s", &simb);
         }
+        if(simb=='x')simb='X';
+        if(simb=='o')simb='O';
 
-        scanf("%d, %d", &L, &C);
-
+    while(velha==1){
+        printf("\nVez de %c!\n", simb);
+        scanf("%d", &L);
+        scanf("%d", &C);
         if(L>3||C>3){
             printf("Coordenada invalida!\n");
             continue;
         }
 
         //definir os elementos e imprimir a matriz
-        for(int l=0; l<LIN; l++){
-            for(int c=0; c<COL; c++){
+        for(int l=0; l<3; l++){
+            for(int c=0; c<3; c++){
                     if(l==L-1&&c==C-1){
                         if(jogo[l][c]=='.'){
-                            if(simb=='x'||simb=='X'){
+                            if(simb=='X'){
                                 printf("|X|");
                                 jogo[l][c]='X';
                             }
-                            if(simb=='o'||simb=='O'){
+                            if(simb=='O'){
                                 printf("|O|");
                                 jogo[l][c]='O';
                             }
                         }
-                        else if(jogo[l][c]!='.'){
+                        else{
                             printf("Essa posicao ja esta ocupada!\n");
-                            continue;
+                            if(simb=='X')simb='O';
+                            else simb='X';
+                            break;
                         }
                     }
                     else{
@@ -69,7 +102,18 @@ void main(){
             printf("\n");
         //fim do for
         }
+        if(simb=='X')simb='O';
+        else simb='X';
+        velha=teste(jogo);
     //fim do while
+    }
+    if(velha!=2){
+        printf("Deu velha!");
+        }
+    else{
+        if(simb=='X')simb='O';
+        else simb='X';
+        printf("Parabens, %c ganhou!", simb);
     }
 //fim do void
 }
